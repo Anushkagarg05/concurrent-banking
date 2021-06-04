b332e59d65f631f6b33e4a6e6168437065959037
Anushkagarg05 <anushkagarg05@gmail.com>
2021-06-04 22:37:30 +0530

    driver/main

---
+++ src/main/java/concurrentbanking/BankSimMain.java
@@ -0,0 +1,31 @@
+package concurrentbanking;
+
+import java.util.concurrent.Semaphore;
+
+public class BankSimMain {
+
+    public static final int NACCOUNTS = 10;
+    public static final int INITIAL_BALANCE = 10000;
+
+    public static void main(String[] args) throws InterruptedException {
+        Bank b = new Bank(NACCOUNTS, INITIAL_BALANCE);
+        Thread[] threads = new Thread[NACCOUNTS];
+
+        // Start a thread for each account.
+        for (int i = 0; i < NACCOUNTS; i++) {
+            threads[i] = new TransferThread(b, i, INITIAL_BALANCE);
+            threads[i].start();
+        }
+        System.out.printf("%-30s Bank transfer is in process.\n", Thread.currentThread().toString());
+
+        // Wait for all threads to complete execution.
+        for(Thread thread : threads) {
+            thread.join();
+        }
+		System.out.printf("FINISHED\n");
+        // Test to see whether the balances have remained the same
+        // After all transactions have completed.
+        b.test();
+          
+    }
+}
